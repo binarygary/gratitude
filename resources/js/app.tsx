8caabe8c8712d5f8898df4cea5116a9160aef420
Gary Kovar <gkovar@gmail.com>
2026-04-01 20:51:48 -0400

    Upgrade app dependencies

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,11 +3,23 @@ import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import type { ComponentType, ReactNode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+type PageComponent = ComponentType & {
+    layout?: ComponentType<{ children: ReactNode }> | ComponentType<{ children: ReactNode }>[] | ((page: ReactNode) => ReactNode) | ((props: unknown) => ReactNode);
+};
+
+type PageModule = {
+    default: PageComponent;
+};
+
 createInertiaApp({
-    resolve: (name) =>
-        resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    resolve: async (name) => {
+        const page = await resolvePageComponent<PageModule>(`./Pages/${name}.tsx`, import.meta.glob<PageModule>('./Pages/**/*.tsx'));
+
+        return page.default;
+    },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
