999b5fd73eb9c8488ebdf22ef93195573230964c
Gary Kovar <gkovar@gmail.com>
2026-04-01 21:47:09 -0400

    fix: make inertia boot config type-safe

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,6 +1,7 @@
 import '../css/app.css';
 import './bootstrap';
 
+import { config } from '@inertiajs/core';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { ComponentType, ReactNode } from 'react';
@@ -14,12 +15,9 @@ type PageModule = {
     default: PageComponent;
 };
 
+config.set('future.useScriptElementForInitialPage', true);
+
 createInertiaApp({
-    defaults: {
-        future: {
-            useScriptElementForInitialPage: true,
-        },
-    },
     resolve: async (name) => {
         const page = await resolvePageComponent<PageModule>(`./Pages/${name}.tsx`, import.meta.glob<PageModule>('./Pages/**/*.tsx'));
 
